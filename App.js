3366838ef69974484124aa1d82ebe1e9d4cbeac8
Nitroceliulioze <aksionova.diana@gmail.com>
2023-07-26 09:40:30 +0300

    route to homepage works

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Homepage from './Home';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   
     return (
-      <View style={styles.container}>
-        <Text>Hello Fabs</Text>
-        <StatusBar style="auto"></StatusBar>
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Globomantics'>
+          <Stack.Screen 
+          name='Globomantics'
+          component={Homepage}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+
     )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+

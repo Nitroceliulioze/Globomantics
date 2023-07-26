9678ac76f43920dd958985d71fbc1521d3403322
Nitroceliulioze <aksionova.diana@gmail.com>
2023-07-26 10:13:34 +0300

    setup header, but not apploading

--- App.js
+++ App.js
@@ -4,22 +4,34 @@ import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Homepage from './Home';
+import { Platform } from 'react-native';
+import { useFonts } from 'expo-font';
+import { AppLoading } from 'expo-app-loading';
+import Header from './Header';
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  
+  let [fontsLoaded] = useFonts({
+    'OpenSans': require('./assets/fonts/OpenSans-Regular.ttf')
+  });
+  // neveikia kazkodel tas async font uzloadinimas ir apploading komponentas
     return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName='Globomantics'>
+      <NavigationContainer 
+        style={{ paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0 }}
+      >
+        <Stack.Navigator 
+        initialRouteName='Globomantics'
+        headerMode='screen'>
           <Stack.Screen 
           name='Globomantics'
           component={Homepage}
+          options={{
+            header: () =>  <Header headerDisplay='Globomantics'/>
+          }}
           />
         </Stack.Navigator>
       </NavigationContainer>
-
-
     )
 }
 
